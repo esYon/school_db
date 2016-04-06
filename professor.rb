e7650a09f9d7eed6d5c98d508549e100d883b5ae
john.h <jholman1993@gmail.com>
2016-04-05 18:30:37 -0700

    Student & professor now inherit from person superclass

--- professor.rb
+++ professor.rb
@@ -1,12 +1,10 @@
-class Professor
-  # first_name,last_name,email,phone,created_at
+require_relative 'person'
+
+class Professor < Person
+  attr_reader :first_name, :last_name, :email, :phone, :created_at
   @@professor_id = 0
   def initialize(args={})
-    @first_name = args[:first_name]
-    @last_name = args[:last_name]
-    @email = args[:email]
-    @phone = args[:phone]
-    @created_at = args[:created_at]
+    super
     @@professor_id += 1
   end
 
--- student.rb
+++ student.rb
@@ -1,12 +1,9 @@
+require_relative 'person'
+
 class Student
   attr_reader :first_name, :last_name, :email, :phone, :created_at
   @@student_id = 0
   def initialize(args={})
-    @first_name = args[:first_name]
-    @last_name = args[:last_name]
-    @email = args[:email]
-    @phone = args[:phone]
-    @created_at = args[:created_at]
     @@student_id += 1
   end
 end
