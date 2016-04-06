3384b74c59ae128f75068f41bb3ffba6c4f9993e
john.h <jholman1993@gmail.com>
2016-04-05 18:31:21 -0700

    ARGV functionality added; MVP

--- runner.rb
+++ runner.rb
@@ -6,14 +6,15 @@ if ARGV.any?
 
 p "Students or Professors?"
 
-if ARGV[0] = 'students'
-  student_parser = PersonParser.new('students.csv')
-  students = student_parser.people
-elsif ARGV[0] = 'professors'
-  professor_parser = PersonParser.new('professors.csv')
-  professors = professor_parser.people
-else
-  "Please enter valid input"
+  if ARGV[0] = 'students'
+    student_parser = PersonParser.new('students.csv')
+    students = student_parser.people
+    students.each { |student| p "#{student.first_name} #{student.last_name} #{student.email} #{student.phone} #{student.created_at} " }
+  elsif ARGV[0] = 'professors'
+    professor_parser = PersonParser.new('professors.csv')
+    professors = professor_parser.people
+    professors.each { |professor| p "#{professor.first_name} #{professor.last_name} #{professor.email} #{professor.phone} #{professor.created_at} " }
+  else
+    "Please enter valid input"
+  end
 end
-
-students.each { |student| p "#{student.first_name} #{student.last_name}" }
