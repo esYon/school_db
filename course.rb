e416b9b10b587c01cc3a21793e534b31538c4ec1
john.h <jholman1993@gmail.com>
2016-04-06 07:56:46 -0700

    Reorganize files

--- course.rb
+++
@@ -1,7 +0,0 @@
-class Course
-  def initialize(args={})
-    @course_id = args[:course_id]
-    @subject = args[:subject]
-    @grade_level = args[:grade_level] || 9..12
-  end
-end
--- student.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'person'
-
-class Student
-  attr_reader :first_name, :last_name, :email, :phone, :created_at
-  @@student_id = 0
-  def initialize(args={})
-    @@student_id += 1
-  end
-end
